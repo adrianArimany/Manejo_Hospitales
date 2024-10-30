04b137233141015d4f03b6977713a1ef63ca661b
adrianArimany <ari211063@uvg.edu.gt>
2024-10-30 15:28:30 -0600

    .

--- src/main/java/com/uvg/proyecto/Classes/Admin.java
+++ src/main/java/com/uvg/proyecto/Classes/Admin.java
@@ -2,15 +2,7 @@ package com.uvg.proyecto.Classes;
 /**
  * This class handles the administration of the hospital:
  * 
- * It meant to have the following functionalities:
- * 1. Add/delete doctors
- * 2. Manage Clinics and their respective offices
- * 3. Manage which doctor has which office from which clinic.
- * 
- * As attributes:
- * -Add/remove clinics (each clinic has a speciality).
- * -Add/remove offices from a clinic
- * 
+ * Currently this Class has no usa whatsoever.....
  * 
  */
 public class Admin {
